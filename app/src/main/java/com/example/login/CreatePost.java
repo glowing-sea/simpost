e624c018b382169c442e4d180009d6db018d076e
Dai Boyu <u7241110@anu.edu.au>
2022-04-25 09:31:36 +1000

    activity_create_post.xml modified CreatePost button script activity_setting_page.xml modified

--- app/src/main/java/com/example/login/CreatePost.java
+++ app/src/main/java/com/example/login/CreatePost.java
@@ -2,13 +2,24 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreatePost extends AppCompatActivity {
-
+    Button posting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+        posting = (Button) findViewById(R.id.btn_CreatePost_posting);
+        posting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
