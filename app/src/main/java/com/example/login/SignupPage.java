bdec2089fcb5e53062f2a335e2c6960c24a6c292
Your Name <uid@anu.edu.au>
2022-04-26 20:02:12 +0800

    login and signup

---
+++ app/src/main/java/com/example/login/SignupPage.java
@@ -0,0 +1,42 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignupPage extends AppCompatActivity {
+    EditText name, password;
+    Button confirm;
+    SharedPreferences userInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_page);
+
+        name = findViewById(R.id.signupname);
+        password = findViewById(R.id.password);
+        confirm = findViewById(R.id.confirmSignup);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirm.setEnabled(false);
+                String n = name.getText().toString();
+                String p = password.getText().toString();
+                SharedPreferences.Editor save = userInfo.edit();
+                save.putString(n, p);
+                save.commit();
+                Toast.makeText(SignupPage.this,"You have successfully signed up", Toast.LENGTH_LONG).show();
+                Intent i = new Intent(SignupPage.this, LoginPage.class);
+                startActivity(i);
+            }
+        });
+    }
+}
