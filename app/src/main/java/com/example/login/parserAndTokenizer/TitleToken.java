c5f14100ccd2b9614e8b02b048f8d050e34c178c
daiboyu <u7241110@anu.edu.au>
2022-05-10 20:26:32 +1000

    simple tokenizer and it's test

---
+++ app/src/main/java/com/example/login/parserAndTokenizer/TitleToken.java
@@ -0,0 +1,23 @@
+package com.example.login.parserAndTokenizer;
+
+public class TitleToken extends Token{
+     String word;
+     Type type;
+    public TitleToken(String word){
+        this.type = Type.TITLE;
+        this.word = word;
+    }
+    @Override
+    public String show() {
+        return this.word;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (o ==null||o.getClass() != this.getClass()){
+            return false;
+        }else {
+            TitleToken token = (TitleToken) o;
+            return (this.word.equals(token.word)&&this.type == ((TitleToken) o).type);        }
+    }
+}
