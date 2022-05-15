5e3dc35295bd5647854b9d9e0ff0370ef3786ab1
Your Name <uid@anu.edu.au>
2022-05-15 19:53:18 +0800

    privacy

--- app/src/main/java/com/example/login/Activity/HomePrivacy.java
+++ app/src/main/java/com/example/login/Activity/HomePrivacy.java
@@ -2,13 +2,20 @@ package com.example.login.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.login.DataContainer.Me;
 import com.example.login.R;
 
+import java.util.ArrayList;
+
 public class HomePrivacy extends AppCompatActivity {
+    Me current = Me.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_privacy);
+        ArrayList<Boolean> privacySettings = current.getPrivacySettings();
     }
+
 }
