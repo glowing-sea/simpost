7ac3ac3d314278c9fe595f71c475c9c4d6c0fd6d
Your Name <uid@anu.edu.au>
2022-05-15 11:15:32 +0800

    createpost images

--- app/src/main/java/com/example/login/Activity/CreatePost.java
+++ app/src/main/java/com/example/login/Activity/CreatePost.java
@@ -1,24 +1,33 @@
 package com.example.login.Activity;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.login.DataContainer.UserAdmin;
 import com.example.login.DataContainer.Post;
 import com.example.login.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.net.URI;
 
 public class CreatePost extends AppCompatActivity {
     private static final String TAG = "Activity_CreatPost";
     private String PRIVATE_DIR;
     Button posting;
     EditText userInput, title;
+    ImageView image1, image2, image3;
+    FloatingActionButton add1, add2, add3;
     UserAdmin current;
     Intent commingIn;
     @Override
@@ -34,6 +43,12 @@ public class CreatePost extends AppCompatActivity {
         title = findViewById(R.id.postTitle);
         userInput = (EditText) findViewById(R.id.multxt_CreatePost_userInput);
         posting = (Button) findViewById(R.id.btn_CreatePost_posting);
+        image1 = findViewById(R.id.post_image_1);
+        image2 = findViewById(R.id.post_image_2);
+        image3 = findViewById(R.id.post_image_3);
+        add1 = findViewById(R.id.add_post_image_1);
+        add2 = findViewById(R.id.add_post_image_2);
+        add3 = findViewById(R.id.add_post_image_3);
         posting.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
@@ -53,5 +68,40 @@ public class CreatePost extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+        add1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 100);
+            }
+        });
+        add2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 101);
+            }
+        });
+        add3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 102);
+            }
+        });
 }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 100){
+            Uri imageURI = data.getData();
+            image1.setImageURI(imageURI);
+        }
+        else if (resultCode == RESULT_OK && requestCode == 101){
+            Uri imageURI = data.getData();
+            image2.setImageURI(imageURI);
+        }
+        else if (resultCode == RESULT_OK && requestCode == 102){
+            Uri imageURI = data.getData();
+            image3.setImageURI(imageURI);
+        }}}
